5c17055141f8395ddd34d90dd7fe3c96cc525cbc
Satell1te <onemoresatelite@gmail.com>
2021-01-31 22:38:01 +1000

    сделал поисковик в чейн вью

--- Storige/ViewPage.swift
+++ Storige/ViewPage.swift
@@ -23,12 +23,47 @@ struct ViewPage: View{ // начало главной структуры
     @State var sortedItems:[Item] = []
     @State var fetchedItems:[Item] = []
     @State var typeOfSorting: Int8 = 1
+    @State private var showCancelButton: Bool = false
+    @State private var searchText = ""
     
     var body: some View // главный вью
     {
-        NavigationView{ // обертка для невигейшн вью начало
+        NavigationView{// обертка для невигейшн вью начало
+            VStack{
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+
+                        TextField("search", text: $searchText, onEditingChanged: { isEditing in
+                            self.showCancelButton = true
+                        }, onCommit: {
+                            print("onCommit")
+                        }).foregroundColor(.primary)
+
+                        Button(action: {
+                            self.searchText = ""
+                        }) {
+                            Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                    .foregroundColor(.secondary)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10.0)
+
+                    if showCancelButton  {
+                        Button("Cancel") {
+                                UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                                self.searchText = ""
+                                self.showCancelButton = false
+                        }
+                        .foregroundColor(Color(.systemBlue))
+                    }
+                }
+                .padding(.horizontal)
+                .navigationBarHidden(showCancelButton)
             List{ // начало оформления списка
-                ForEach(sortedItems) {  Item in // для каждого предмета в списке SortedItems применяем эти оформления
+                ForEach(sortedItems.filter{$0.serialNum!.hasPrefix(searchText) || searchText == ""}) {  Item in // для каждого предмета в списке SortedItems применяем эти оформления
                     Button(action: { //начало действий при нажатии кнопки
                         activeSheet = .second // вызываем шит с подробностями об объекте
                         hernya.sharedUuid = Item.itemid
@@ -59,8 +94,10 @@ struct ViewPage: View{ // начало главной структуры
                     updateArrays()
                 } //отклик и обработка удаления предмета в спике конец
             } // конец оформления списка
+            }
             .listStyle(PlainListStyle())//модификатор для списка
             .navigationBarTitle("Обзор", displayMode: .automatic)//настройки для топ бара навигации
+            .resignKeyboardOnDragGesture()
             .navigationBarItems(leading: Button(action:{sortSheet.toggle()},label: {Text("Сортировка")}),//первая строчка кнопки в топ баре добавления нового предмета
             trailing: Button(action: {activeSheet = .first}, label: {Image(systemName: "plus.circle").imageScale(.large)}))//вторая строчка кнопки в топ баре добавления нового предмета
             .sheet(item: $activeSheet) { item in //шит с добавлением, или же обзором предмета
@@ -119,3 +156,27 @@ struct hernya{ // херня начало
     static var sharedSerialNum = ""
     static var sharedAmount: Int64 = 1
 } // херня конец
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
